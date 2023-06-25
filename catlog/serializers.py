cd7f62f6fd5fd00bae5d213da985e7256eb40602
Hemant <hphero272@gmail.com>
2023-06-25 13:37:30 +0530

    refiend apis of main,sub,product

--- catlog/serializers.py
+++ catlog/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import ProdImages,ProdReview,Products,SubCateorgy,MainCateorgy
+from .models import ProdImage,ProdReview,Product,SubCateorgy,MainCateorgy
+from accounts.models import User
+from accounts.serializers import UserSerializer
 
 class MainCateorgySerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,18 +12,39 @@ class SubCateorgySerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCateorgy
         fields = "__all__"
+        depth = 0
+    
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     data['main_cateorgy'] = MainCateorgySerializer(
+    #         MainCateorgy.objects.get(pk=data['main_cateorgy'])).data
+    #     return data
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Products
+        model = Product
         fields = "__all__"
+        depth = 0
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['images'] = [i['image'] for i in ProdImageSerializer(ProdImage.objects.filter(image_fk = data['id']),many=True).data]
+        return data
 
 class ProdReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProdReview
         fields = "__all__"
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['review_fk'] = ProductSerializer(
+            Product.objects.get(pk=data['review_fk'])).data
+        data['reviewed_by'] = UserSerializer(
+            User.objects.get(pk=data['reviewed_by'])).data
+        return data
 
-class ProdImagesSerializer(serializers.ModelSerializer):
+class ProdImageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ProdImages
-        fields = "__all__"
+        model = ProdImage
+        fields = ["image"]
+        depth = 0
