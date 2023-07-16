c06b3e11aca2c736b9aeaff2f9724ba1932018c7
Dhruvin3103 <dcchawda2003@gmail.com>
2023-07-16 22:51:04 +0530

    cry !!!

--- core/settings.py
+++ core/settings.py
@@ -164,6 +164,12 @@ STATIC_URL = 'static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "staticfiles" # new
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
