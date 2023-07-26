20fc74aa10c65170dd03fff75fe78d82b3cd1f36
dhruvin <dhruvinhemant5@gmail.com>
2023-07-26 03:31:25 -0700

    bhdvghsv

--- core/settings.py
+++ core/settings.py
@@ -104,26 +104,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 #     # 'default': {
 #     #     'ENGINE': 'django.db.backends.postgresql',
 #     #     'NAME': config('NAME'),
 #     #     'USER': config('USER'),
 #     #     'PASSWORD': config('PASSWORD'),
 #     # }
+# }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(config('DATABASE_URL'))
+# }
+
+DATABASES = {
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
-
-# DATABASES = {
-#     'default': dj_database_url.parse(config('DATABASE_URL'))
-# }
-
-# DATABASES = {
-#     'default': dj_database_url.parse(config('DATABASE_URL'))
-# }
 
 
 # Password validation
